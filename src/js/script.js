01b402731291bf3361da7443b0fe92e82a8faf1d
Awerock <cigan-yashka@yandex.ru>
2023-05-19 00:53:05 +0300

    new: function players

--- src/js/script.js
+++ src/js/script.js
@@ -16,5 +16,118 @@ window.addEventListener('scroll', () => {
         earth.style.width = value * 0.3 + 873 + `px`;
         earth.style.marginTop = value * -0.3 + `px`;
     }
-    console.log(value);
 })
+
+const players = [
+    {
+    name: "Eugene Chi", 
+    username: "Awerock", 
+    img: "./img/me.jpg",
+    games: ["Apex", "FIFA"], 
+    handle: "@Awerock" 
+    },
+    {
+        name: "Daniel Yefimov", 
+        username: "Patrick_INCorp",
+        img: "none", 
+        games: ["Apex"], 
+        handle: "@INC_PatrIck" 
+    },
+    {
+        name: "Igor Baranov", 
+        username: "Vindemaitrix",
+        img: "none", 
+        games: ["Apex"], 
+        handle: "@_sqpo_1206" 
+    },
+    {
+        name: "Mikhail Bolyabkin", 
+        username: "I'M MIHA, SORRY!",
+        img: "./img/bolyabkin.jpg", 
+        games: ["Apex"], 
+        handle: "@bolyabkin" 
+    },
+    {
+        name: "Andrey Feschenko", 
+        username: "andreeew94",
+        img: "none", 
+        games: ["FIFA"], 
+        handle: "@andreeew94" 
+    },
+  ];
+
+const createGameCard = function (game) {
+    const gamePlayers = players.filter(player => player.games.includes(game) || game === 'all');
+    const cardContainer = document.getElementById('cardContainer');
+
+    gamePlayers.forEach(player => {
+            const contentItem = document.createElement('div');
+            contentItem.classList.add('team__content-item');
+            cardContainer.appendChild(contentItem);
+    
+            const contentImgBox = document.createElement('div');
+            contentImgBox.classList.add('team__content-img');
+            contentItem.appendChild(contentImgBox);
+    
+            const contentImg = document.createElement('img');
+            contentImg.src = player.img;
+            contentImg.alt = player.name;
+            contentImgBox.appendChild(contentImg);
+    
+            const contentName = document.createElement('h2');
+            contentName.classList.add('team__content-username');
+            contentName.textContent = player.name;
+            contentItem.appendChild(contentName);
+    
+            const contentHandle = document.createElement('small');
+            contentHandle.classList.add('team__content-handle');
+            contentHandle.textContent = player.handle;
+            contentItem.appendChild(contentHandle);
+    
+            const contentButtons = document.createElement('div');
+            contentButtons.classList.add('team__content-buttons');
+            contentItem.appendChild(contentButtons);
+    
+            const contentButtonsInfo = document.createElement('button');
+            contentButtonsInfo.classList.add('team_button-alt');
+            contentButtonsInfo.classList.add('team_button-alt__primary');
+            contentButtonsInfo.textContent = 'More info';
+            contentButtons.appendChild(contentButtonsInfo);
+    
+            const contentButtonsShare = document.createElement('button');
+            contentButtonsShare.classList.add('team_button-alt');
+            contentButtonsShare.classList.add('team_button-alt__icon');
+            contentButtonsShare.innerHTML = `<i class="ph-export"></i>`;
+            contentButtons.appendChild(contentButtonsShare);
+        });
+};
+
+createGameCard('all');
+
+const allButton = document.getElementById('allButton');
+allButton.addEventListener("click", (event) => {
+    cardContainer.innerHTML = "";
+    createGameCard('all');
+    allButton.classList.add("team__button-active");
+    fifaButton.classList.remove("team__button-active");
+    apexButton.classList.remove("team__button-active");
+});
+
+const fifaButton = document.getElementById('fifaButton');
+fifaButton.addEventListener("click", (event) => {
+    cardContainer.innerHTML = "";
+    createGameCard('FIFA');
+    allButton.classList.remove("team__button-active");
+    fifaButton.classList.add("team__button-active");
+    apexButton.classList.remove("team__button-active");
+});
+
+const apexButton = document.getElementById('apexButton');
+apexButton.addEventListener("click", (event) => {
+    cardContainer.innerHTML = "";
+    createGameCard('Apex');
+    allButton.classList.remove("team__button-active");
+    fifaButton.classList.remove("team__button-active");
+    apexButton.classList.add("team__button-active");
+});
+
