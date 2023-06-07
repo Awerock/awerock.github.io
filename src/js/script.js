ba730840b9fe21318ef15f358709af43722e77b6
Awerock <cigan-yashka@yandex.ru>
2023-06-07 16:15:55 +0300

    add: adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -3,20 +3,24 @@ let miniStar = document.getElementById('parallax__mini-stars');
 let bigStar = document.getElementById('parallax__big-stars');
 let luna = document.getElementById('parallax__luna');
 let earth = document.getElementById('parallax__earth');
+let earthWidht = earth.clientWidth;
 
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
+    let screenWidth = window.innerWidth;
 
-    if (value > 800) {
-        value = 0;
-    } else {
-        logo.style.marginTop = value * 0.5 + `px`;
-        miniStar.style.marginTop = value * 1.5 + `px`;
-        bigStar.style.marginTop = value * 0.5 + `px`;
-        earth.style.width = value * 0.3 + 873 + `px`;
-        earth.style.marginTop = value * -0.3 + `px`;
+    if (screenWidth >= 1000) {
+        if (value > 800) {
+            value = 0;
+        } else {
+            logo.style.marginTop = value * 0.5 + 'px';
+            miniStar.style.marginTop = value * 1.5 + 'px';
+            bigStar.style.marginTop = value * 0.5 + 'px';
+            earth.style.width = value * 0.3 + earthWidht + 'px';
+            earth.style.marginTop = value * -0.3 + 'px';
+        }
     }
-})
+});
 
 const players = [
     {
